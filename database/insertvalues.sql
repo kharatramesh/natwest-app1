bbaba5bfd5488562d764a3abb79f40562e26dfa8
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2024-10-14 15:31:43 +0530

    Update insertvalues.sql

--- database/insertvalues.sql
+++ database/insertvalues.sql
@@ -5,3 +5,4 @@ INSERT INTO priarworld (firstname, rollnumber) VALUES ('Rutuja',4);
 INSERT INTO priarworld (firstname, rollnumber) VALUES ('Tanmay',5);
 INSERT INTO priarworld (firstname, rollnumber) VALUES ('Rohit',6);
 INSERT INTO priarworld (firstname, rollnumber) VALUES ('Rohit',7);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Rohit1',8);
